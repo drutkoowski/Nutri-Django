164ecf821784512751e39ccf7c0ede9458b35eee
drutkoowski <d.rutkowski2000@gmail.com>
2022-10-13 23:57:35 +0200

    final layout changes

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -2,7 +2,8 @@ const hamburgerNav = document.getElementById('navi-toggle')
 const navigationList = document.getElementsByClassName('navigation__list')[0]
 const navigationNavDiv = document.getElementsByClassName('navigation__nav')[0]
 
-
+const gainsVideo = document.getElementById('gains-video')
+const goalsVideo = document.getElementById('goals-video')
 
 hamburgerNav.addEventListener('click', () => {
     console.log(hamburgerNav.checked)
@@ -13,8 +14,8 @@ hamburgerNav.addEventListener('click', () => {
     else if (navigationList.classList.contains('not-visible') && hamburgerNav.checked) {
          navigationList.classList.remove('not-visible')
     }
-
-
 })
 
 
+gainsVideo.playbackRate = 0.75;
+goalsVideo.playbackRate = 0.75;
