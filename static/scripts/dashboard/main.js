8d1e7bd69e49f9e08bc1ede14a02da463af231c6
drutkoowski <d.rutkowski2000@gmail.com>
2022-10-17 23:13:31 +0200

    adjusting dashboard charts

--- static/scripts/dashboard/main.js
+++ static/scripts/dashboard/main.js
@@ -1,9 +1,11 @@
 let myChart = document.getElementById('summaryChart').getContext('2d')
-let weekChart = document.getElementById('summaryWeekly')
-
-let data = {
-  labels: [
-  ],
+let weekChart = document.getElementById('summaryWeekly').getContext('2d')
+let nutritionDetails = document.getElementById('summaryNutritionDetails').getContext('2d')
+const xValuesWeekGraph = ["Mon","Tuesday","Wed","Thr","Fri","Sat","Sun"];
+let detailsColors = []
+let nutritionDetailsDataset = [60, 40, 30, 20, 90, 83, 63, 33, 3]
+let dataSummary = {
+  labels: [],
   datasets: [
     {
       data: [60, 40],
@@ -17,10 +19,8 @@ let data = {
       // ]
     }]
 };
-const xValues = ["Mon","Tuesday","Wed","Thr","Fri","Sat","Sun"];
-
-let data2 = {
-    labels: xValues,
+let dataWeekGraph = {
+    labels: xValuesWeekGraph,
     datasets: [{
       data: [2860, 2140, 3060, 2777, 1895, 2345, 2753],
       borderColor: "green",
@@ -28,9 +28,47 @@ let data2 = {
 };
 
 
-let summaryChart = new Chart(myChart, {
+
+$.each(nutritionDetailsDataset, function (index, value) {
+    if (value <= 20) {
+        detailsColors[index] = "rgba(199,239,201,0.6)"
+    }
+    if(value > 20 && value < 40) {
+        detailsColors[index] = "rgba(199,239,201,1)"
+    }
+    else if(value >= 40 && value < 60) {
+        detailsColors[index] = "rgba(123,241,129, 0.6)"
+    }
+    else if(value >= 60 && value < 80) {
+        detailsColors[index] = "rgba(123,241,129, 0.8)"
+    }
+    else if(value >= 80 && value <=100) {
+        detailsColors[index] = "rgba(123,241,129, 1)"
+    }
+})
+
+
+let nutritionDetailsData = {
+    labels: [
+        "Fiber", "Saturated Fat", "Cholesterol", "Sodium", "Sugar", "Potassium", "Iron", "Calcium", "Magnesium"
+    ],
+    datasets: [
+    {
+      data: nutritionDetailsDataset,
+      backgroundColor: detailsColors,
+
+      // hoverBackgroundColor: [
+      //   "#2ce04f",
+      //   "#c1e5b7",
+      // ]
+    }]
+}
+
+
+
+let summaryChartBox = new Chart(myChart, {
   type: 'doughnut',
-  data: data,
+  data: dataSummary,
   options: {
   	responsive: true,
     maintainAspectRatio: false,
@@ -44,7 +82,7 @@ let summaryChart = new Chart(myChart, {
 
 let weekChartBox = new Chart(weekChart, {
   type: 'line',
-  data: data2,
+  data: dataWeekGraph,
   options: {
   	responsive: true,
     maintainAspectRatio: false,
@@ -55,6 +93,32 @@ let weekChartBox = new Chart(weekChart, {
         }
   }
 });
+
+let nutritionDetailsChartBox = new Chart(nutritionDetails, {
+    type: 'bar',
+    data: nutritionDetailsData,
+    options: {
+        scales: {
+            x: {
+            suggestedMin: 0,
+            suggestedMax: 100,
+            }
+        },
+        indexAxis: 'y',
+        elements: {
+          bar: {
+            borderWidth: 2,
+          }
+        },
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false
+          },
+        }
+  }
+})
+
 
 // summaryChart.canvas.parentNode.style.height = '15rem';
 // summaryChart.canvas.parentNode.style.width = '15rem';
