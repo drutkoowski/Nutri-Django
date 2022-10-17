34020826066f0f03056f2f3d00585c86e02efd8d
drutkoowski <d.rutkowski2000@gmail.com>
2022-10-17 19:44:04 +0200

    adding weekly calorie graph to dashboard

--- static/scripts/dashboard/main.js
+++ static/scripts/dashboard/main.js
@@ -1,4 +1,5 @@
-let myChart = document.getElementById('myChart').getContext('2d')
+let myChart = document.getElementById('summaryChart').getContext('2d')
+let weekChart = document.getElementById('summaryWeekly')
 
 let data = {
   labels: [
@@ -10,10 +11,19 @@ let data = {
         "#8be085",
         "#c8e3c8",
       ],
-      hoverBackgroundColor: [
-        "#2ce04f",
-        "#c1e5b7",
-      ]
+      // hoverBackgroundColor: [
+      //   "#2ce04f",
+      //   "#c1e5b7",
+      // ]
+    }]
+};
+const xValues = ["Mon","Tuesday","Wed","Thr","Fri","Sat","Sun"];
+
+let data2 = {
+    labels: xValues,
+    datasets: [{
+      data: [2860, 2140, 3060, 2777, 1895, 2345, 2753],
+      borderColor: "green",
     }]
 };
 
@@ -25,10 +35,24 @@ let summaryChart = new Chart(myChart, {
   	responsive: true,
     maintainAspectRatio: false,
     cutout: 50,
-    // borderRadius: 30,
+    events: [],
     legend: {
-      display: false
+      display: false,
     }
+  }
+});
+
+let weekChartBox = new Chart(weekChart, {
+  type: 'line',
+  data: data2,
+  options: {
+  	responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+            legend: {
+                display: false
+            },
+        }
   }
 });
 
