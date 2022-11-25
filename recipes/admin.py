2bbc28535649c280dbd3213204ee869d75e75697
drutkoowski <d.rutkowski2000@gmail.com>
2022-11-25 20:43:29 +0100

    developing models for recipe

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Recipe
+
 
 # Register your models here.
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("name_pl", 'name_en', "person_count", "difficulty_pl", 'difficulty_en','author', 'duration', 'ingredients_pl', 'steps_pl', 'steps_en', 'ingredients_en')
+
+
+admin.site.register(Recipe, RecipeAdmin)
+
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Recipe(models.Model):
+    name_pl = models.CharField(max_length=100,default='')
+    name_en = models.CharField(max_length=100)
+    person_count = models.IntegerField(blank=True, null=True)
+    difficulty_pl = models.CharField(max_length=50, blank=True, null=True)
+    difficulty_en = models.CharField(max_length=50, blank=True, null=True)
+    author = models.CharField(max_length=100, blank=True, null=True)
+    duration = models.CharField(max_length=30, blank=True, null=True)
+    ingredients_pl = models.JSONField(blank=True,default=dict)
+    ingredients_en = models.JSONField(blank=True,default=dict)
+    steps_pl = models.JSONField(blank=True, default=dict)
+    steps_en = models.JSONField(blank=True, default=dict)
