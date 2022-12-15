613298db177e8895fd037ec5c063b578f9c19eeb
drutkoowski <d.rutkowski2000@gmail.com>
2022-12-15 17:26:39 +0100

    customazing front page

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,45 +1,85 @@
-const allSections = Array.from(document.querySelectorAll('.section'))
-const allSectionsY = Array.from(document.querySelectorAll('.section'))
-const allYArr = []
-allSectionsY.forEach(section => {
-  const y = section.getBoundingClientRect().top
-  allYArr.push(y)
-})
+const htmlStart = document.querySelector('html')
+htmlStart.style.overflow = 'hidden'
+document.addEventListener("DOMContentLoaded", function(){
 
-let index = 0
-const animationDuration = 500
-let lastTime = 0
-
-$(window).bind('mousewheel', function(event) {
-  const currentTime = new Date().getTime()
-   if (currentTime - lastTime < animationDuration){
-      event.preventDefault()
-      return
-   }
-   else {
-     if (event.originalEvent.wheelDelta >= 0) {
-        console.log('Scroll up');
-        if (index >= 0){
-        index = index - 1
-        console.log(allYArr[index])
-        window.scrollTo({
-          top: allYArr[index],
-          behavior: 'smooth',
+    htmlStart.style.removeProperty('overflow')
+    let isFirstScroll = true
+    window.onresize = function(){ location.reload(); }
+    const allVideos = document.querySelectorAll('.bg-video')
+    if (allVideos){
+         allVideos.forEach(video => {
+            const videoEl = video.getElementsByTagName('video')[0]
+            videoEl.playbackRate = 0.75
         })
-      }
-      }
-      else {
-          console.log('Scroll down');
-          if (index < 5){
-            index = index + 1
-            console.log(allYArr[index])
+    }
+
+
+    const html = document.querySelector('html')
+    // html.style.backgroundColor = 'rgba(11,1,1,1)'
+    html.classList.add('no-scroll')
+    // const lastPos = document.body.getBoundingClientRect().top
+    const allSections = Array.from(document.querySelectorAll('.section'))
+    const allYArr = []
+    allSections.forEach(section => {
+      const y = section.getBoundingClientRect().top
+      allYArr.push(y)
+    })
+
+    let index = 0
+    const animationDuration = 50
+    let lastTime = 0
+
+    $(window).bind('mousewheel', function(event) {
+       const currentTime = new Date().getTime()
+       if (currentTime - lastTime < animationDuration && !isFirstScroll){
+          return
+       }
+       else {
+         isFirstScroll = false
+         if (event.originalEvent.wheelDelta >= 0) {
+             console.log('up')
+            if (index >= 0){
+            index = index - 1
             window.scrollTo({
               top: allYArr[index],
               behavior: 'smooth',
             })
           }
-      }
-      lastTime = currentTime
-   }
+          }
+          else {
+              if (index < 5){
+                   console.log('down')
+                index = index + 1
+                window.scrollTo({
+                  top: allYArr[index],
+                  behavior: 'smooth',
+                })
+              }
 
+          }
+          lastTime = currentTime
+       }
+      // console.clear()
+    });
+    const arrowUp = document.querySelector('.scroll-up-arrow')
+    arrowUp.addEventListener('click', () => {
+      index = 0
+       window.scrollTo({
+         top: allYArr[index],
+         behavior: 'smooth',
+       })
+      // console.clear()
+    })
+
+    const startingPos = html.getBoundingClientRect().top
+    if (startingPos < 50){
+      index = 0
+       window.scrollTo({
+         top: allYArr[index],
+         behavior: 'smooth',
+       })
+      // console.clear()
+    }
 });
+
+
